c6bd43550bbd44f858ce1c6e1b6bfd8be20a4d65
Breno Santana Bruno <brenosantana@devstorm.io>
2024-03-09 08:59:48 -0300

    0.0.2

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("TWITCH_ACCESS_TOKEN não definido no arquivo .env");
     let client_id = env::var("TWITCH_CLIENT_ID")
         .expect("TWITCH_CLIENT_ID não definido no arquivo .env");
-    let category_name = "33214"; // Substitua "NomeDaCategoria" pela categoria desejada
-
+    // let category_name = "33214"; // Substitua "NomeDaCategoria" pela categoria desejada | Fortnite: 33214
+    let category_name = "21779";
     let client = reqwest::Client::new();
-    let url = format!("https://api.twitch.tv/helix/streams?game_id={}", category_name);
+    let url = format!("https://api.twitch.tv/helix/streams?game_id={}&first=2&language=pt", category_name);
+
+
+    // let url = format!("https://api.twitch.tv/helix/streams?game_id=21779?first=100");
+
 
     let response = client.get(&url)
         .header("Authorization", format!("Bearer {}", access_token))
@@ -39,9 +43,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let body = response.text().await?;
         let twitch_response: TwitchResponse = serde_json::from_str(&body)?;
 
-        println!("Streams na categoria '{}':", category_name);
+        println!("Streams na categoria '{}':", category_name); // #TODO Converter ID da categoria para nome
         for stream in twitch_response.data {
-            println!("Usuário: {}, Título: {}", stream.user_name, stream.title);
+            println!("Usuário: {}, Título: {}", stream.user_name, stream.title); // #TODO adicionar paginação* para mais resultados
         }
     } else {
         println!("Erro: {}", response.status());
