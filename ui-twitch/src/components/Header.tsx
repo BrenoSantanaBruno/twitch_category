b8e6d1684a5582932e224381b0a2d37f07ae4885
GamerCleanVic <jonassd2000@gmail.com>
2024-03-09 15:04:01 -0300

    flex, colors, etcTailwind

---
+++ ui-twitch/src/components/Header.tsx
@@ -0,0 +1,91 @@
+const Header = ()=>{
+    return(
+        <div className="
+            home-ui
+            flex
+            flex-row
+            justify-around
+            items-center
+            flex-wrap
+        ">
+            <div className="
+                logo-ui
+                flex
+                flex-row
+                justify-around
+                items-center
+                flex-wrap
+                ml-2
+                mr-[3rem]
+                border-b-4
+                border-indigo-500
+            ">
+                <p>
+                    <a href="#">DevStorm</a>
+                </p>
+            </div>
+            <div className="
+                assistir-ui
+                header-ui
+                flex
+                flex-row
+                justify-around
+                items-center
+                flex-wrap
+                ml-4
+                mr-[3rem]
+            ">
+                <p className="
+                    item-menu
+                    ml-2
+                    mr-2
+                    border-b-4
+                  border-indigo-500
+                "><a href="#">Assistir</a></p>
+                <p className="
+                    item-menu
+                    ml-2
+                    mr-2
+                    border-b-4
+                  border-indigo-500
+                "><a href="#">Sobre</a></p>
+            </div>
+            <div className="
+                github-ui
+                header
+                flex
+                flex-row
+                justify-around
+                items-center
+                flex-wrap
+                ml-4
+                mr-2
+            ">
+                <p className="
+                    item-menu
+                    ml-2
+                    mr-2
+                    border-b-4
+                  border-indigo-500
+                "><a href="#">GitHub</a></p>
+                <p className="
+                    item-menu
+                    ml-2
+                    mr-2
+                    border-b-4
+                  border-indigo-500
+                "><a href="#">Discord</a></p>
+            </div>
+            <div className="
+                mt-3
+                border-b-2
+                border-purple-500
+                w-[100%]
+            ">
+
+            </div>
+        </div>
+    );
+}
+
+export default Header;
