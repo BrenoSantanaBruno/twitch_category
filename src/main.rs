8d09a8326153afece3acf89b5b7c47f06cf82b64
Breno Santana Bruno <brenosantana@devstorm.io>
2024-03-09 09:31:28 -0300

    0.0.3

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ struct TwitchStream {
     user_name: String,
     game_id: String,
     title: String,
+    game_name: String,
 }
 
 #[tokio::main]
@@ -24,14 +25,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("TWITCH_ACCESS_TOKEN não definido no arquivo .env");
     let client_id = env::var("TWITCH_CLIENT_ID")
         .expect("TWITCH_CLIENT_ID não definido no arquivo .env");
-    // let category_name = "33214"; // Substitua "NomeDaCategoria" pela categoria desejada | Fortnite: 33214
-    let category_name = "21779";
+    let category_name = "21779"; // ID da categoria desejada
+    let pagination = 100;
+    let language = "pt";
     let client = reqwest::Client::new();
-    let url = format!("https://api.twitch.tv/helix/streams?game_id={}&first=2&language=pt", category_name);
-
-
-    // let url = format!("https://api.twitch.tv/helix/streams?game_id=21779?first=100");
-
+    let url = format!("https://api.twitch.tv/helix/streams?game_id={}&first={}&language={}", category_name , pagination, language);
 
     let response = client.get(&url)
         .header("Authorization", format!("Bearer {}", access_token))
@@ -43,10 +41,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let body = response.text().await?;
         let twitch_response: TwitchResponse = serde_json::from_str(&body)?;
 
-        println!("Streams na categoria '{}':", category_name); // #TODO Converter ID da categoria para nome
+        println!("Streams na categoria '{}':", category_name);
+        let mut user_count = 0; // Contador de resultados de usuário
         for stream in twitch_response.data {
-            println!("Usuário: {}, Título: {}", stream.user_name, stream.title); // #TODO adicionar paginação* para mais resultados
+            println!("Usuário: {}, Título: {}, Game: {}", stream.user_name, stream.title, stream.game_name);
+            user_count += 1; // Incrementa o contador de resultados de usuário
         }
+        println!("Total de resultados de usuário: {}", user_count); // Imprime o total de resultados de usuário
     } else {
         println!("Erro: {}", response.status());
         if let Ok(body) = response.text().await {
