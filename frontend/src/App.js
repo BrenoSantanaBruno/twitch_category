d3ccef1aef7e84346b2c790750db53aa4abefd07
Breno Santana Bruno <brenosantana@devstorm.io>
2024-03-12 21:57:44 -0300

    Alterando estilização

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import axios from 'axios';
+
 function TwitchStreams() {
     const [streams, setStreams] = React.useState([]);
 
     React.useEffect(() => {
-            const fetchData = async () => {
-                try {
-                    const response = await axios.get('http://localhost:8080/api/twitch-streams');
-                    setStreams(response.data.data);
-                } catch (error) {
-                    console.error('Erro ao buscar os dados: ', error);
-                }
-            };
-            fetchData();
-        },
-        []);
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/api/twitch-streams');
+                setStreams(response.data.data);
+            } catch (error) {
+                console.error('Erro ao buscar os dados: ', error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div>
-            <h1>Streams da Twitch</h1>
-            <ul>
-                {streams.map(stream => (
-                    <li key={stream.user_id}>
-                        <h2>{stream.user_name}</h2>
-                        <p>Título: {stream.title}</p>
-                        <p>Categoria: {stream.game_name}</p>
-                        <p>Viewers: {stream.viewer_count}</p>
+            <h1 className="title">Twitch Streams</h1>
+            <div className="stream-container">
+                {streams.map((stream, index) => (
+                    <a key={stream.user_id} href={`https://www.twitch.tv/${stream.user_name}`} target="_blank" rel="noopener noreferrer" className="stream-card" style={{ animationDelay: `${index * 0.1}s` }}>
                         <img src={stream.thumbnail_url.replace('{width}', '300').replace('{height}', '300')} alt={stream.user_name} />
-                    </li>
+                        <div className="stream-details">
+                            <h2>{stream.user_name}</h2>
+                            <p>Título: {stream.title}</p>
+                            <p>Categoria: {stream.game_name}</p>
+                            <p>Viewers: {stream.viewer_count}</p>
+                        </div>
+                    </a>
                 ))}
-            </ul>
+            </div>
         </div>
     );
 }
@@ -38,6 +41,7 @@ function App() {
         <div className="App">
             <TwitchStreams />
         </div>
-);
-    }
+    );
+}
+
 export default App;
